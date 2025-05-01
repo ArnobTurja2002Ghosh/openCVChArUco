42d18282ca213e7ad127e165245e201ccc67f297
Arnob <arnobg2002@gmail.com>
2025-05-01 16:09:24 -0230

    json naming style changed

--- json1.py
+++ json1.py
@@ -1,5 +1,8 @@
 import json
-
+import os
+PATH_TO_YOUR_IMAGES = './calibration_images'
+image_files = [os.path.join(PATH_TO_YOUR_IMAGES, f) for f in os.listdir(PATH_TO_YOUR_IMAGES) if f.endswith(".nef")]
+image_files.sort()  # Ensure files are in order
 def writeUpLookatEye(i, matrix):
     #print("writing", i, "matrix")
     with open('camShader.json', 'r') as openfile:
@@ -9,8 +12,9 @@ def writeUpLookatEye(i, matrix):
     json_object['Data']['Up'] = matrix[:, 0].tolist()
     json_object['Data']['Lookat'] = matrix[:, 1].tolist()
     json_object['Data']['Eye'] = matrix[:, 2].tolist()
+    json_object['Data']['Name']= image_files[i][len(PATH_TO_YOUR_IMAGES+"/"):-4]
     #print(json_object)
-    with open('camShader_'+str(i)+'.json', 'w') as outfile:
+    with open('camShader'+json_object['Data']['Name']+'.json', 'w') as outfile:
         # Writing to json file
         json.dump(json_object, outfile, indent=4)
 
