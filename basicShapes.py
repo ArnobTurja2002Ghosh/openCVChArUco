faa3d4c407f6080aef750b4b3f98c0a95f3fd05d
Arnob <arnobg2002@gmail.com>
2025-07-18 10:06:29 -0230

    boundary

--- basicShapes.py
+++ basicShapes.py
@@ -3,9 +3,10 @@ import numpy as np
 
 # Create a blank white image
 width, height = 819, 455
-image = np.ones((height, width, 3), dtype=np.uint8) * 255  # White background
+
 
 def basic_shapes():
+    image = np.ones((height, width, 3), dtype=np.uint8) * 255  # White background
     # Draw red triangle (R)
     triangle_cnt = np.array([[0.25*width/2, 0.75*height/2], [width/3, 0.25*height/2], [width/2, 0.75*height/2]], dtype=np.int32)
     cv2.drawContours(image, [triangle_cnt], 0, (0, 0, 255), -1)  # Red fill (BGR)
@@ -24,38 +25,16 @@ def basic_shapes():
     cv2.imwrite("basic_shapes.png", image)
 
 def boundary():
-    pt1 = (0, 0)
-    pt2=(0, height)
-    for i in range(0, int(width/16), 5):  
-        pt1 = (i, 0)
-        pt2 = (i, height) 
-        cv2.line(image,pt1,pt2,(0,0,255),1)
-        
-    top_left = tuple(map(int, (width-((width/16)/2)/10, 0) ))
-    bottom_right = tuple(map(int, (width, height) ))
-    for i in range(10):
-        color1 = (list(np.random.choice(range(256), size=3)))  
-        color =[int(color1[0]), int(color1[1]), int(color1[2])]  
-        cv2.rectangle(image, top_left, bottom_right, color, -1)
-        bottom_right = tuple(map(int, (top_left[0], bottom_right[1]) ))
-        top_left = tuple(map(int, (top_left[0] - ((width/16)/2)/10, top_left[1]) ))
-    top_left = tuple(map(int, (0, 0) ))
-    bottom_right = tuple(map(int, (width, ((height/9)/2)/10) ))   
-    for i in range(10):
-        color1 = (list(np.random.choice(range(256), size=3)))  
-        color =[int(color1[0]), int(color1[1]), int(color1[2])]  
-        cv2.rectangle(image, top_left, bottom_right, color, -1)
-        top_left = tuple(map(int, (top_left[0], bottom_right[1]) ))
-        bottom_right = tuple(map(int, (bottom_right[0], bottom_right[1] + ((height/9)/2)/10) ))
-    
-    bottom_right = tuple(map(int, (width, height) ))  
-    top_left = tuple(map(int, (0, height-((height/9)/2)/10) ))  
-    for i in range(10):
-        color1 = (list(np.random.choice(range(256), size=3)))  
-        color =[int(color1[0]), int(color1[1]), int(color1[2])]  
-        cv2.rectangle(image, top_left, bottom_right, color, -1)
-        bottom_right = tuple(map(int, (bottom_right[0], top_left[1]) ))
-        top_left = tuple(map(int, (top_left[0], top_left[1] - ((height/9)/2)/10) ))
+    image = np.zeros((height, width, 3), dtype=np.uint8)  # Black background
+
+    # Draw a white border on all sides
+    cv2.rectangle(
+        image,
+        (0, 0),
+        (width - 1, height - 1),
+        (255, 255, 255),
+        thickness=1
+    )
     cv2.imwrite("boundary.png", image)
 
 boundary()
